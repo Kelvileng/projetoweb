2acf2129db48de6f9f41a1f7ad08b9693b5e89fe
Kelvileng <kelvilens@outlook.com>
2021-11-26 23:38:58 +0000

    o

--- projetoweb/src/App.js
+++ projetoweb/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './componentes/Herder';
 import Footer from './componentes/Footer';
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom';
 import  Routes from './Routes';
 import './App.css';
 
--- projetoweb/src/Routes.js
+++ projetoweb/src/Routes.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-
-export default () => {
-    return(
-        <Switch>
-            <Route exact path="/" >
-                primeira pag
-            </Route>
-            
-            <Route exact path="/config" >
-                config
-            </Route>
-        </Switch>
-    );
-}
+ import { Switch ,Route } from 'react-router-dom';
+  import Inicio from './Paginas/Inicio';
+   import Config from './Paginas/Config';
+    export default () => {
+         return(
+              <Switch>
+                   <Route path="/" >
+                        <Inicio />
+                         primeira pag
+                          </Route>
+                           <Route path="/Config"> 
+                           <Config /> 
+                           </Route>
+                            </Switch>
+                             );
+                              }
